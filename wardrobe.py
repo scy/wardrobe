d3e14d02e94175f600391b93daa4a6d15eddf5d1
Tim Weber <tim.weber@dotsunited.de>
2010-04-15 10:26:45 +0200

    Locker.ensure() will raise instead of exiting.

--- wardrobe.py
+++ wardrobe.py
@@ -7,6 +7,9 @@ import sys
 import tempfile
 
 class Locker:
+	class AcquireError(StandardError):
+		"""The requested lock could not be acquired."""
+		pass
 	path = None
 	locked = False
 	def __init__(self, ensurenow = False, directory = 'wardrobe.lock.d'):
@@ -28,8 +31,11 @@ class Locker:
 		atexit.register(self.unlockIfLocked)
 		return (True)
 	def ensure(self):
+		"""Try to lock and raise an `AcquireError` if this fails."""
 		if not self.lock():
-			sys.exit('Could not acquire lock.')
+			raise self.AcquireError(
+				"could not acquire lock '%s'" % self.path
+			)
 	def unlock(self):
 		if not self.locked:
 			raise StateError('not locked, cannot unlock')
